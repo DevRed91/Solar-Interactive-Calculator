bd5da5b2be826e2479abc2f27fc023962907affd
Devraj Reddy <51876334+DevRed91@users.noreply.github.com>
2026-03-13 16:28:18 +0530

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+// next.config.ts
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {};
+
+module.exports = withPWA(nextConfig);
