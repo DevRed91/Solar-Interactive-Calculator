a5feeea816294f687d4c74f185dc3985e767514a
Devraj Reddy <51876334+DevRed91@users.noreply.github.com>
2026-04-08 06:59:21 +0000

    Thank you page updated

--- src/components/thank-you/Left.tsx
+++ src/components/thank-you/Left.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 import Image from 'next/image';
 import {
   CheckedIcon,
@@ -18,11 +18,11 @@ const ThankYouLeft = ({ status }: IThankYouLeft) => {
   const { name } = useSolarStore();
   const { dictionary } = useTranslation();
   const { counter, resetAndGoHome } = useAutoRedirection();
-
+  const isVersion2 = false;
   return (
-    <div className="flex h-full w-full flex-col justify-between gap-8 bg-[url('/images/background/step-nine.webp')] bg-top bg-repeat-x p-14">
+    <div className="flex h-full w-full flex-col justify-between gap-8 bg-[url('/images/background/step-nine.webp')] bg-top bg-repeat-x p-15">
       <div className="flex flex-col gap-8">
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
           <div className="bg-green-success-500 mb-2 flex h-[94px] w-[94px] items-center justify-center overflow-hidden rounded-full p-2">
             <CheckedIcon className="h-14 w-14" />
           </div>
@@ -33,7 +33,7 @@ const ThankYouLeft = ({ status }: IThankYouLeft) => {
             content={dictionary.thankYou.left.title as TextItem[]}
           />
         </div>
-        <div className="border-b-black-100 flex items-center justify-between border-b pb-[43px]">
+        <div className="border-b-black-100 flex w-[1176px] items-center justify-between border-b pb-[35px]">
           {status?.map((item, index) => (
             <div key={index} className="flex items-center gap-4">
               {item.checked ? (
@@ -59,49 +59,91 @@ const ThankYouLeft = ({ status }: IThankYouLeft) => {
             </div>
           ))}
         </div>
-        <div className="flex flex-col gap-6">
-          <div className="text-black-300 font-dm-sans text-[32px]/[45px] font-semibold -tracking-[1.28px]">
-            {dictionary.thankYou.left.expectSection.title}
+        {isVersion2 ? (
+          <div className="flex flex-col gap-6">
+            <div className="text-black-300 font-dm-sans text-[32px]/[45px] font-semibold -tracking-[1.28px]">
+              Here's what you can{' '}
+              <span className="font-dm-sans text-neutral-500">
+                expect in the next 20 Minutes
+              </span>{' '}
+              at the Experience Centre:
+            </div>
+            <div className="flex gap-6">
+              {dictionary.thankYou.left.expectSection.data?.map(
+                (item, index) => (
+                  <div
+                    key={index}
+                    className="border-background-400 flex w-[438px] flex-col items-center overflow-hidden rounded-xl border"
+                  >
+                    <MixColorsText
+                      content={item.content}
+                      className="z-[1] overflow-visible p-5 pb-0"
+                      contentClassName="!text-2xl/[34px] !font-dm-sans -tracking-[0.96px] whitespace-pre-wrap"
+                    />
+
+                    <div className="relative h-full w-full">
+                      <Image
+                        height={0}
+                        width={0}
+                        sizes="100vw"
+                        src={item.image}
+                        alt={item.image}
+                        className="h-full w-full object-fill"
+                      />
+                      <div className="absolute -top-6 left-0 h-[100px] w-full rotate-180 bg-linear-(--thank-you-gradient)" />
+                    </div>
+                  </div>
+                ),
+              )}
+            </div>
           </div>
-          <div className="grid grid-cols-2 gap-6">
-            {dictionary.thankYou.left.expectSection.data?.map((item, index) => (
-              <div
-                key={index}
-                className="border-background-400 flex h-[118px] items-center overflow-hidden rounded-xl border"
-              >
-                <div className="relative h-full min-w-fit">
-                  <Image
-                    height={0}
-                    width={0}
-                    sizes="100vw"
-                    src={item.image}
-                    alt={item.image}
-                    className="h-full w-max max-w-[167px] object-cover"
-                  />
-                  <div className="absolute -inset-y-5 -right-5 w-28 bg-linear-(--thank-you-gradient)" />
-                </div>
-                <MixColorsText
-                  content={item.content}
-                  className="z-[1]"
-                  contentClassName="!text-2xl/[34px] !font-dm-sans -tracking-[0.96px]"
-                />
-              </div>
-            ))}
+        ) : (
+          <div className="flex flex-col gap-6">
+            <div className="text-black-300 font-dm-sans text-[32px]/[45px] font-semibold -tracking-[1.28px]">
+              {dictionary.thankYou.left.expectSection.title}
+            </div>
+            <div className="flex h-[330px] gap-6">
+              {dictionary.thankYou.left.expectSection.data2?.map(
+                (item, index) => (
+                  <div
+                    key={index}
+                    className="border-background-400 flex w-[438px] flex-col items-center overflow-hidden rounded-xl border p-5 pt-0"
+                  >
+                    <div className="relative min-w-fit">
+                      <Image
+                        height={0}
+                        width={0}
+                        sizes="100vw"
+                        src={item.image}
+                        alt={item.image}
+                        className="h-[220px] w-[290px]"
+                      />
+                      <div className="absolute -bottom-5 left-0 h-[100px] w-full bg-linear-(--thank-you-gradient)" />
+                    </div>
+                    <MixColorsText
+                      content={item.content}
+                      className="z-[1]"
+                      contentClassName="!text-2xl/[34px] !font-dm-sans -tracking-[0.96px]"
+                    />
+                  </div>
+                ),
+              )}
+            </div>
           </div>
-        </div>
+        )}
       </div>
       <div className="flex w-[90%] items-center gap-6">
         <Button
           variant="primary"
-          content={dictionary['thankYou'].right.buttons.home}
+          content={dictionary['thankYou'].left.buttons.home}
           leftIcon={
             <SmallRightArrowIcon className="fill-background-50 h-10 w-10 rotate-180" />
           }
           onClick={resetAndGoHome}
-          className="!w-1/2"
+          className="!w-[512px]"
         />
-        <p className="text-black-400 font-dm-sans [&>*]:text-[28px]/[39px] [&>span]:font-normal">
-          {renderStyledText(dictionary.thankYou.right.redirectNote, {
+        <p className="text-black-400 font-dm-sans w-[446px] [&>*]:text-[28px]/[39px] [&>span]:font-normal">
+          {renderStyledText(dictionary.thankYou.left.redirectNote, {
             seconds: counter.toString(),
           })}
         </p>
